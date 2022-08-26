21a23e64c1d9fffb2c9bb90277bfb98dd7188450
전휘찬 <jeonhwichan@jeonhwichan-ui-MacBookPro.local>
2022-08-26 19:41:58 +0900

    testing c with first mac settings

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(void){
 
-    cout << "hello world!\n";
+    vector<int> test{1, 2, 3, 4, 5};
+    sort(test.begin(), test.end(), greater<int>());
+    for(int elem : test){
+        cout << elem << " ";
+    }
     return 0;
 }
