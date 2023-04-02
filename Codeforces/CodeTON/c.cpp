f08ead8b5ec015c57d1228ff1914531328d534b6
Hwichan Jeon <gnlcks82@gmail.com>
2023-04-03 03:34:10 +0900

    removed .DS_Store from git

--- Codeforces/CodeTON/c.cpp
+++ Codeforces/CodeTON/c.cpp
@@ -22,7 +22,7 @@ ll lmax(ll a, ll b){
  
 void solve(){
     int n;
-    ll c, d, add = 0;    
+    ll c, d, add = 0;
     cin >> n >> c >> d;
     vector<ll> a(n);
     for (int i = 0; i < n; i++) {
@@ -35,32 +35,44 @@ void solve(){
             add += c;
         }
     }
-    a.push_back(LLONG_MAX);
+    a.push_back(0);
     sort(a.begin(), a.end());
     vector<ll> live(n, 0), die(n, 0);
-    int dpIdx = 1, start = 0;
     for (int i = 0; i < n; i++){
         live[i] = die[i] = 0;
     }
-
+    int dpIdx = 1, start = 0;
     ll last;
     for (int i = 1; i < a.size(); i++){
-        if (a[i] == LLONG_MAX) break;
-        if (a[i + 1] != a[i] + 1 && (start || a[i + 1] == LLONG_MAX)){
+        if (a[i] == LLONG_MAX)
+            break;
+        last = a[i];
+        if (a[i] != a[i - 1] + 1 && start){
             live[dpIdx] = live[dpIdx - 1] + d * (a[start] - a[start - 1] - 1);
-            die[dpIdx] = min(die[dpIdx - 1], live[dpIdx - 1]) + c * (a[i] - a[start] + 1);
+            die[dpIdx] = min(live[dpIdx - 1], die[dpIdx - 1]) + c * (a[i] - a[start] - 1);
             dpIdx++;
             start = 0;
         }
-        else if (a[i + 1] != a[i] + 1){
+        else if (a[i] != a[i - 1] + 1){
             start = i;
         }
     }
+    // 마지막에 2번째 끊어진 구간이 없는 경우
+    // 식 전체에서 단 한번도 끊어진 구간이 없어 start = 0인 경우 -> add == answer (고려 x)
     if (start){
         live[dpIdx] = live[dpIdx - 1] + d * (a[start] - a[start - 1] - 1);
-        die[dpIdx] = min(die[dpIdx - 1], live[dpIdx - 1]) + c * (last - a[start] + 1);
+        die[dpIdx] = min(die[dpIdx - 1], live[dpIdx - 1]) + c * (last - a[start] - 1);
         dpIdx++;
     }
+    cout << "live : ";
+    for (int i = 0; i < dpIdx; i++){
+        cout << live[i] << " ";
+    }
+    cout << "\n dead : ";
+    for (int i = 0; i < dpIdx; i++){
+        cout << die[i] << " ";
+    }
+    cout << "\n";
     cout << add + lmin(live[dpIdx - 1], die[dpIdx - 1]) << "\n";
 }
  
